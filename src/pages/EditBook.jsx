acf7f0b06893da3eb23139cc45824802d7d6f4ee
Shubhamnagar21 <119088961+Shubhamnagar21@users.noreply.github.com>
2025-01-28 03:01:54 +0530

    Update EditBook.jsx

--- src/pages/EditBook.jsx
+++ src/pages/EditBook.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import BackButton from "../components/BackBUtton";
+import BackButton from "../components/BackButton";
 import axios from "axios";
 import Spinner from "../components/Spinner";
 import { useNavigate, useParams } from "react-router-dom";
